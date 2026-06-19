a901ff98beb03c75c665253934918bfa7eeab524
OldWhiteWolf <96644135+OldWhiteWolf@users.noreply.github.com>
2026-06-19 08:44:36 -0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -129,5 +129,5 @@ app.post("/submit", upload.array("documents", 10), async (req, res) => {
 });
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT,"0.0.0.0", () => console.log(`✅ TriVault backend running on port ${PORT}`));
